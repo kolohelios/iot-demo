3c123e1a315ae9337497f9bf7daa7d25f6a7bf82
Jon Edwards <jkedwards@me.com>
2019-07-08 06:56:34 -0700

    add microservices suite to docker-compose
    create Dockerfile for microservices
    add PM2 ecosystem.config.js
    amend levelGenerator to consume Docker services

---
+++ services/microservices/ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: 'levelGenerator',
+      script: './node_modules/.bin/ts-node',
+      args: './levelGenerator/index.ts',
+      watch: ['./levelGenerator/**/*'],
+      env: {
+        NODE_ENV: 'development',
+      },
+    },
+  ],
+}
--- services/microservices/levelGenerator/index.ts
+++ services/microservices/levelGenerator/index.ts
@@ -5,22 +5,18 @@ import { promisify } from 'util'
 const readFileAsync = promisify(readFile)
 
 const connect = async (): Promise<void> => {
-  // TODO change to use /run/secrets when the dev tooling is updated
   const applicationIDSecret = (await readFileAsync(
-    '../secrets/parse_server_application_id'
-    // '/run/secrets/parse_server_application_id'
+    '/run/secrets/parse_server_application_id'
   ))
     .toString()
     .replace('\n', '')
   const masterKey = (await readFileAsync(
-    // TODO change to use /run/secrets when the dev tooling is updated
-    '../secrets/parse_server_master_key'
-    // '/run/secrets/parse_server_master_key'
+    '/run/secrets/parse_server_master_key'
   ))
     .toString()
     .replace('\n', '')
   Parse.initialize(applicationIDSecret, masterKey)
-  ;(Parse as any).serverURL = 'http://localhost:1337/parse'
+  ;(Parse as any).serverURL = 'http://services_parse_1:1337/parse'
 
   const Levels = Parse.Object.extend('level')
   const query = new Parse.Query(Levels)
