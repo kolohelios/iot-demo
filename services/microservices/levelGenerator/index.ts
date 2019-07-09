824f0480f28c23912ab32b9cfa0b3e575514d1fe
Jon Edwards <jkedwards@me.com>
2019-07-08 21:59:39 -0700

    publish messages using mqtt
    wire up levelGenerator to MQTT broker
    create new levels objects if they don't exist when starting up levelGenerator
    add interval to publish messages
    add mqtt and uuid as dependencies
    disable autorestart for levelGenerator

--- services/microservices/levelGenerator/index.ts
+++ services/microservices/levelGenerator/index.ts
@@ -1,10 +1,16 @@
 import { readFile } from 'fs'
+import * as mqtt from 'mqtt'
 import * as Parse from 'parse/node'
 import { promisify } from 'util'
+import * as uuidv1 from 'uuid/v1'
 
 const readFileAsync = promisify(readFile)
 
 const connect = async (): Promise<void> => {
+  const client = mqtt.connect('mqtt://services_mqtt_1', { port: 1883 })
+  await new Promise((fulfilled) => {
+    client.on('connect', fulfilled)
+  })
   const applicationIDSecret = (await readFileAsync(
     '/run/secrets/parse_server_application_id'
   ))
@@ -21,8 +27,32 @@ const connect = async (): Promise<void> => {
   const Levels = Parse.Object.extend('level')
   const query = new Parse.Query(Levels)
 
-  const levels = await query.find()
-  console.log(levels)
+  let levels = await query.find()
+
+  if (!levels.length) {
+    const newLevels = []
+    for (let i = 0; i < 3; i++) {
+      // TODO add type for levels and use it here
+      const newLevel: any = new Parse.Object('level')
+      newLevel.set('levelId', uuidv1())
+      newLevels.push(newLevel)
+    }
+
+    levels = await Parse.Object.saveAll(newLevels)
+  }
+
+  const levelIdArray = levels.map((level) => {
+    return level.get('levelId')
+  })
+
+  setInterval(() => {
+    levelIdArray.forEach((levelId) => {
+      client.publish(
+        `level/${levelId}/item`,
+        Math.floor(Math.random() * 100).toString()
+      )
+    })
+  }, 2 * 1000)
 }
 
 // TODO handle the error if it's a missing secret by throwing a meaningful error
